7673b573bc6bd30da0618f8326334e99830ad4cd
Hrithwin <hrithwin123@gmail.com>
2025-07-16 23:36:22 +0530

    ccc

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ import express from "express"
 const app = express()
 
 app.set("trust proxy", true)
+app.use(express.json())
 
 app.get("/", (req, res) => {
     console.log(`Users Ip is : ${req.ip}`)
+
+    return res.json({success : true, message : "ip scanned"})
 })
 
 app.listen(3000, () => console.log("app listening on port 3000"))
